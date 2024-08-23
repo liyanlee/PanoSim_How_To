c4c826b3cc747a2cbd5dc97eec059d9de92e2d37
pengfei.hu <pengfei.hu@panosim.com>
2024-08-23 10:18:40 +0800

    new create Sensor/Camera/MonoCamera

---
+++ Sensor/Camera/MonoCamera/PanoSimDatabase/Plugin/Agent/HowTo_MonoCamera.py
@@ -0,0 +1,23 @@
+from DataInterfacePython import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ModelStart(userData):
+    OutputFormat = "time@i,%d@[,r@b,g@b,b@b" % (1280 * 720)
+    userData["sensor"] = BusAccessor(userData["busId"], 'MonoCameraSensor.0', OutputFormat)
+    userData['last_time'] = 0
+    plt.ion()
+    plt.figure(dpi=100).canvas.set_window_title('PanoSim HowTo Sensor: Mono Camera')
+
+def ModelOutput(userData):
+    timestamp, _ = userData['sensor'].readHeader()
+    if timestamp > userData['last_time']:
+        userData['last_time'] = timestamp
+        plt.clf()
+        plt.title('Sensor: Mono Camera')
+        plt.imshow(np.frombuffer(userData["sensor"].getBus()[8:], dtype=np.uint8).reshape((720, 1280, 3)))
+        plt.pause(interval=0.0001)
+
+def ModelTerminate(userData):
+    plt.ioff()
+    plt.close()
