d39be127c4833cdb498a787f07a42d99fed971f3
pengfei.hu <pengfei.hu@panosim.com>
2024-09-03 09:44:04 +0800

    new create Bus/traffic_object_highlight

---
+++ Bus/traffic_object_highlight/PanoSimDatabase/Plugin/Agent/HowTo_Bus_traffic_object_highlight.py
@@ -0,0 +1,14 @@
+from DataInterfacePython import *
+
+def ModelStart(userData):
+    Format = 'time@i,100@[,id@i,r@b,g@b,b@b,a@b'
+    userData["bus"] = BusAccessor(userData["busId"], "traffic_object_highlight", Format)
+
+def ModelOutput(userData):
+    id, red, green, blue, alpha = 109, 127, 0, 0, 127
+    if userData['time'] > 2000:
+        userData['bus'].writeHeader(*(userData['time'], 1))
+        userData['bus'].writeBody(0, *(id, red, green, blue, alpha))
+
+def ModelTerminate(userData):
+    pass
